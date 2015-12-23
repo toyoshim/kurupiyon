f6dc912ea7e38f2f3d00e5a97b058636447bacdd
Takashi Toyoshima <toyoshim@gmail.com>
2015-12-23 11:26:24 +0000

    Add logging

--- web.js
+++ web.js
@@ -1,11 +1,29 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var momolog = require("momolog");
+
 var app = express();
 
-app.use(logfmt.requestLogger());
-app.use(express.static(__dirname + '/static'));
+var url = process.env.MONGOLAB_URI;
+if (url) {
+  var log = momolog();
+  var collection = process.env.DEBUG ? 'log-debug' : 'log';
+  console.log('col: ' + collection);
+  log.connect(url, collection).then(function() {
+    app.use(log.morgan());
+    main();
+  });
+} else {
+  main();
+}
 
-var port = Number(process.env.PORT);
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+
+function main() {
+  app.use(logfmt.requestLogger());
+  app.use(express.static(__dirname + '/static'));
+
+  var port = Number(process.env.PORT);
+  app.listen(port, function() {
+      console.log("Listening on " + port);
+  });
+}
